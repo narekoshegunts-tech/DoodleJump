974018f0443540f6203789461eaaadd811bea906
Narek <narench@inbox.ru>
2026-04-30 15:06:40 +0300

    + поменял input system. Теперь управление мышкой или тач скрином вместо кнопок A,D

--- Assets/Scripts/Gameplay/Player/PlayerMover.cs
+++ Assets/Scripts/Gameplay/Player/PlayerMover.cs
@@ -12,33 +12,60 @@ namespace Gameplay.Player
         private Rigidbody2D _rigidbody;
         private float _lastDirection;
 
+        private float _maxPositionX;
+        private float _minPositionX;
+
         public event Action<float> DirectionChanged;
         
         private void Awake()
         {
             _input = new InputSystem();
             _rigidbody = GetComponent<Rigidbody2D>();
+            
+            SetMinMaxPositionX();
+        }
+
+        private void SetMinMaxPositionX()
+        {
+            var camera = UnityEngine.Camera.main;
+            _minPositionX = camera.ViewportToWorldPoint(Vector3.zero).x;
+            _maxPositionX = camera.ViewportToWorldPoint(Vector3.one).x;
         }
 
         private void Update()
         {
             CheckChangedDirection();
+            Move();
         }
 
         private void CheckChangedDirection()
         {
-            var direction = _input.Player.Move.ReadValue<float>();
+            var direction = _input.Player.Move.ReadValue<Vector2>().x;
             
-            if (!Mathf.Approximately(direction, _lastDirection))
+            if ((direction > 0 && _lastDirection < 0) || 
+                (direction < 0 && _lastDirection > 0))
             {
                 ChangeDirection(direction);
             }
+            
             _lastDirection = direction;
         }
-        
+
+        private void Move()
+        {
+            transform.Translate(Vector2.right * (speed * _lastDirection * Time.deltaTime));
+            if (transform.position.x > _maxPositionX)
+            {
+                transform.position = new Vector2(_maxPositionX, transform.position.y);
+            }
+            else if (transform.position.x < _minPositionX)
+            {
+                transform.position = new Vector2(_minPositionX, transform.position.y);
+            }
+        }
+
         private void ChangeDirection(float direction)
         {
-            _rigidbody.linearVelocity = new Vector3(direction * speed, _rigidbody.linearVelocity.y);
             DirectionChanged?.Invoke(direction);
         }
 
